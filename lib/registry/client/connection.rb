37ed74dbfceef8c8ceb62d6b8d051260e1590b77
mose <mose@mose.com>
2014-01-02 00:56:03 +0800

    tuning on connection

--- lib/registry/client/connection.rb
+++ lib/registry/client/connection.rb
@@ -8,17 +8,16 @@ module Registry
       def initialize(params)
         @conn = Faraday.new "https://#{params['host']}:#{params['port']}",
           :ssl => {
+            ## will need proper config
             :verify => false
           }
       end
 
       def ping
-        begin
-          response = @conn.get '/ping'
-          response.body
-        rescue Exception => e
-          e.message
-        end
+        response = @conn.get '/ping'
+        response.body
+      rescue Exception => e
+        e.message
       end
 
     end
